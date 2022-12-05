aa0882e069913d67cc931443d3e5e8381e10fd92
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2022-12-05 11:09:10 +0000

    Initial change to deploy CIC BE stack

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+stack_name="$1"
+
+if [ -z "$stack_name" ]
+then
+echo "😱 stack name expected as first argument, e.g. ./deploy di-ipv-cri-cic"
+exit 1
+fi
+
+sam validate -t infrastructure/lambda/template.yaml
+sam build -t infrastructure/lambda/template.yaml
+sam deploy --stack-name "$stack_name" \
+   --no-fail-on-empty-changeset \
+   --no-confirm-changeset \
+   --resolve-s3 \
+   --region eu-west-2 \
+   --capabilities CAPABILITY_IAM \
+   --parameter-overrides \
+   CodeSigningEnabled=false \
+   Environment=dev \
+   AuditEventNamePrefix=/common-cri-parameters/CicAuditEventNamePrefix \
+   CriIdentifier=/common-cri-parameters/CicCriIdentifier \
+   CommonStackName=cic-common-cri-api-local \
+   SecretPrefix=cic-cri-api
