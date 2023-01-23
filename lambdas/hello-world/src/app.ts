a48bfd62eceb692ff74f0b70bcb7c048a734b141
Maruf Ahmed <maruf.ahmed@digital.cabinet-office.gov.uk>
2023-01-23 11:08:00 +0000

    Modified app.ts

--- lambdas/hello-world/src/app.ts
+++ lambdas/hello-world/src/app.ts
@@ -1,21 +1,17 @@
 
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import {LambdaInterface} from '@aws-lambda-powertools/commons';
 
-import {
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult
-  }
-    from 'aws-lambda/trigger/api-gateway-proxy'
-  // interface Person {
-  //     name: string
-  //     surname: string
-  //     age: number
-  // }
-  export const lambdaHandler = async (
-    event: APIGatewayProxyEvent
-  ): Promise<APIGatewayProxyResult> => {
-    const queries = JSON.stringify(event.queryStringParameters)
-    return {
-      statusCode: 200,
-      body: `Queries: ${queries}`
+
+class AccessTokenLambda implements LambdaInterface {
+    public async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
+        console.log("Hello world!");
+        return {
+            statusCode: 200,
+            body: `Hello world`
+        };
     }
-  }
+}
+
+const handlerClass = new AccessTokenLambda();
+export const lambdaHandler = handlerClass.handler.bind(handlerClass);
