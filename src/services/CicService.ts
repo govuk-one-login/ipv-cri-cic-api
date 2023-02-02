0dcf2916207a142b3af96e5420a9352f4d122d3c
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2023-02-02 18:01:37 +0000

    implement common lib

--- src/services/CicService.ts
+++ src/services/CicService.ts
@@ -1,131 +1,108 @@
 /* eslint-disable no-console */
 import { CicSession } from "../models/CicSession";
 import { SessionItem } from "../models/SessionItem";
-import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { AppError, SessionNotFoundError } from "../utils/AppError";
-import { StatusCodes } from "http-status-codes";
-import { createDynamoDbClient } from "../utils/aws-client-factory";
-import { DynamoDBDocument, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { createDynamoDbClient } from "../utils/DynamoDBFactory";
+import { DynamoDBDocument, GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { ExternalCode } from "../vendor/ExternalCode";
+
 
 export class CicService {
-  readonly tableName: string;
+    readonly tableName: string;
 
-  private readonly dynamo: DynamoDBDocument;
+    private readonly dynamo: DynamoDBDocument;
 
-  readonly logger: Logger;
+    readonly logger: Logger;
 
-  private static instance: CicService;
+    private static instance: CicService;
 
-  constructor(tableName: any, logger: Logger) {
-  	// throw error if tableName iss null
-  	this.tableName = tableName;
-  	this.dynamo = createDynamoDbClient();
-  	this.logger = logger;
-  }
+    private static externalInstance: ExternalCode;
 
-  static getInstance(tableName: string, logger: Logger): CicService {
-  	if (!CicService.instance) {
-  		CicService.instance = new CicService(tableName, logger);
-  	}
-  	return CicService.instance;
-  }
+    constructor(tableName: any, logger: Logger) {
+    	this.tableName = tableName;
+    	this.dynamo = createDynamoDbClient();
+    	this.logger = logger;
+    	CicService.externalInstance = new ExternalCode();
+    }
 
-  async getSessionById(sessionId: string): Promise<SessionItem | undefined> {
-  	//const tableName = await this.configService.getSessionTableName();
-  	const getSessionCommand = new GetCommand({
-  		TableName: this.tableName,
-  		Key: {
-  			sessionId,
-  		},
-  	});
-  	let session;
-  	try {
-  		session = await this.dynamo.send(getSessionCommand);
-  	} catch (e: any) {
-  		this.logger.error("getSessionById - failed executing get from dynamodb: " + e);
-  		throw new AppError("Error retrieving Session", 500);
-  	}
 
-  	if (!session.Item) {
-  		throw new SessionNotFoundError(`Could not find session item with id: ${sessionId}`);
-  	}
-  	return new SessionItem(session.Item);
-  }
+    static getInstance(tableName: string, logger: Logger): CicService {
+    	if (!CicService.instance) {
+    		CicService.instance = new CicService(tableName, logger);
+    	}
+    	return CicService.instance;
+    }
 
-  // async getSessionById(sessionId: string): Promise<SessionItem | undefined> {
-  // 	let session;
-  // 	this.logger.debug("Table name " + this.tableName);
-  // 	this.logger.debug("Session id " + sessionId);
-  // 	const params = {
-  // 		TableName: this.tableName,
-  // 		Key: {
-  // 			sessionId,
-  // 		},
-  // 	};
-  // 	try {
-  // 		session = await this.dynamo.get(params).promise();
-  // 	}
-  //
-  // 	if (!session?.Item) {
-  // 		this.logger.error("no session found");
-  // 		throw new AppError(StatusCodes.NOT_FOUND, "Session was not found");
-  // 	}
-  // 	return new SessionItem(session.Item);
-  //
-  // }
+    async getSessionById(sessionId: string): Promise<SessionItem | undefined> {
+    	this.logger.debug("Table name ", this.tableName);
+    	const getSessionCommand = new GetCommand({
+    		TableName: this.tableName,
+    		Key: {
+    			sessionId,
+    		},
+    	});
+    	let session;
+    	try {
+    		session = await this.dynamo.send(getSessionCommand);
+    	} catch (e: any) {
+    		this.logger.error("getSessionById - failed executing get from dynamodb: " + e);
+    		throw new AppError("Error retrieving Session", 500);
+    	}
 
-  async saveCICData(sessionId: string, cicData: CicSession): Promise<void> {
-  	this.logger.debug(sessionId);
-  	const params: any = {
-  		TableName: this.tableName,
-  		Key: {
-  			sessionId,
-  		},
-  		UpdateExpression: "set fullName = :fullName, dateOfBirth = :dateOfBirth, documentSelected = :documentSelected, dateOfExpiry =:dateOfExpiry",
+    	if (!session.Item) {
+    		throw new SessionNotFoundError(`Could not find session item with id: ${sessionId}`);
+    	}
+    	return new SessionItem(session.Item);
+    }
 
-  		ExpressionAttributeValues: {
-  			":fullName": cicData.fullName,
-  			":dateOfBirth": cicData.dateOfBirth,
-  			":documentSelected": cicData.documentSelected,
-  			":dateOfExpiry": cicData.dateOfExpiry,
-  		},
-  	};
+    async saveCICData(sessionId: string, cicData: CicSession): Promise<void> {
+    	this.logger.debug(sessionId);
 
-  	this.logger.info("updateItem - updating CIC data in dynamodb" + JSON.stringify(params));
-  	try {
-  		await this.dynamo.update(params);
-  		this.logger.info("updateItem - updated CIC data in dynamodb" + JSON.stringify(params));
-  	} catch (error) {
-  		this.logger.error("got error " + error);
-  		throw new AppError("updateItem - failed ", 500);
-  	}
-  }
+    	const saveCICCommand: any = new UpdateCommand({
+    		TableName: this.tableName,
+    		Key: { sessionId },
+    		UpdateExpression: "SET fullName = :fullName, dateOfBirth = :dateOfBirth, documentSelected = :documentSelected, dateOfExpiry =:dateOfExpiry",
 
-  async setAuthorizationCode(sessionId: string, uuid: string): Promise<void> {
-  	this.logger.debug(sessionId);
+    		ExpressionAttributeValues: {
+    			":fullName": cicData.fullName,
+    			":dateOfBirth": cicData.dateOfBirth,
+    			":documentSelected": cicData.documentSelected,
+    			":dateOfExpiry": cicData.dateOfExpiry,
+    		},
+    	});
 
-  	const params: any = {
-  		TableName: this.tableName,
-  		Key: {
-  			sessionId,
-  		},
-  		UpdateExpression: "set authorizationCode = :authorizationCode, authorizationCodeExpiryDate = :authorizationCodeExpiryDate",
+    	this.logger.info("updateItem - updating CIC data in dynamodb" + JSON.stringify(saveCICCommand));
+    	try {
+    		await this.dynamo.send(saveCICCommand);
+    		this.logger.info("updateItem - updated CIC data in dynamodb" + JSON.stringify(saveCICCommand));
+    	} catch (error) {
+    		this.logger.error("got error " + error);
+    		throw new AppError("updateItem - failed ", 500);
+    	}
+    }
 
-  		ExpressionAttributeValues: {
-  			":authorizationCode": uuid,
-  			":authorizationCodeExpiryDate": "1894981200",
-  		},
-  	};
+    async setAuthorizationCode(sessionId: string, uuid: string): Promise<void> {
+    	this.logger.debug(sessionId);
 
-  	this.logger.debug("updateItem - updating authorizationCode dynamodb" + JSON.stringify(params));
+    	const updateSessionCommand = new UpdateCommand({
+    		TableName: this.tableName,
+    		Key: { sessionId },
+    		UpdateExpression: "SET authorizationCode=:authCode, authorizationCodeExpiryDate=:authCodeExpiry",
+    		ExpressionAttributeValues: {
+    			":authCode": uuid,
+    			":authCodeExpiry": CicService.externalInstance.getAuthorizationCodeExpirationEpoch(),
+    		},
+    	});
 
-  	try {
-  		await this.dynamo.update(params);
-  		this.logger.info("updateItem - updated authorizationCode in dynamodb" + JSON.stringify(params));
-  	} catch (e: any) {
-  		this.logger.error("got error " + e);
-  		throw new AppError("updateItem - failed ", 500);
-  	}
-  }
+    	this.logger.debug("updateItem - updating authorizationCode dynamodb" + JSON.stringify(updateSessionCommand));
+
+    	try {
+    		await this.dynamo.send(updateSessionCommand);
+    		this.logger.info("updateItem - updated authorizationCode in dynamodb" + JSON.stringify(updateSessionCommand));
+    	} catch (e: any) {
+    		this.logger.error("got error " + e);
+    		throw new AppError("updateItem - failed ", 500);
+    	}
+    }
 }
--- src/services/RequestProcessor.ts
+++ src/services/RequestProcessor.ts
@@ -1,6 +1,5 @@
 import { CicSession } from "../models/CicSession";
 import { Response } from "../utils/Response";
-import { StatusCodes } from "http-status-codes";
 import { CicService } from "./CicService";
 import { Metrics, MetricUnits } from "@aws-lambda-powertools/metrics";
 import { randomUUID } from "crypto";
@@ -9,68 +8,69 @@ import { Logger } from "@aws-lambda-powertools/logger";
 import { ValidationHelper } from "../utils/ValidationHelper";
 import { CicResponse } from "../utils/CicResponse";
 import { AppError } from "../utils/AppError";
+import { HttpCodesEnum } from "../utils/HttpCodesEnum";
 
 const SESSION_TABLE = process.env.SESSION_TABLE;
 
 export class RequestProcessor {
-  private static instance: RequestProcessor;
+	private static instance: RequestProcessor;
 
-  private readonly logger: Logger;
+	private readonly logger: Logger;
 
-  private readonly metrics: Metrics;
+	private readonly metrics: Metrics;
 
-  private readonly validationHelper: ValidationHelper;
+	private readonly validationHelper: ValidationHelper;
 
-  private readonly cicService: CicService;
+	private readonly cicService: CicService;
 
-  constructor(logger: Logger, metrics: Metrics) {
-	  if (!SESSION_TABLE) {
-		  logger.error("Environment variable SESSION_TABLE is not configured");
-		  throw new AppError( "Service incorrectly configured", 500);
-	  }
-  	this.logger = logger;
-  	this.validationHelper = new ValidationHelper();
-  	this.metrics = metrics;
-  	this.cicService = CicService.getInstance(SESSION_TABLE, this.logger);
-  }
+	constructor(logger: Logger, metrics: Metrics) {
+		if (!SESSION_TABLE) {
+			logger.error("Environment variable SESSION_TABLE is not configured");
+			throw new AppError( "Service incorrectly configured", 500);
+		}
+		this.logger = logger;
+		this.validationHelper = new ValidationHelper();
+		this.metrics = metrics;
+		this.cicService = CicService.getInstance(SESSION_TABLE, this.logger);
+	}
 
-  static getInstance(logger: Logger, metrics: Metrics): RequestProcessor {
-  	if (!RequestProcessor.instance) {
-  		RequestProcessor.instance = new RequestProcessor(logger, metrics);
-  	}
-  	return RequestProcessor.instance;
-  }
+	static getInstance(logger: Logger, metrics: Metrics): RequestProcessor {
+		if (!RequestProcessor.instance) {
+			RequestProcessor.instance = new RequestProcessor(logger, metrics);
+		}
+		return RequestProcessor.instance;
+	}
 
-  async processRequest(event: APIGatewayProxyEvent, sessionId: string): Promise<Response> {
-  	let cicSession;
-  	try {
-		  this.logger.debug("IN processRequest");
-  		const bodyParsed = JSON.parse(event.body as string);
-  		cicSession = new CicSession(bodyParsed);
-  		await this.validationHelper.validateModel(cicSession, this.logger);
-  		this.logger.debug("CIC Session is  " + JSON.stringify(cicSession));
-  	} catch (error) {
-  		return new Response(StatusCodes.BAD_REQUEST, "Missing mandatory fields in the request payload");
-  	}
+	async processRequest(event: APIGatewayProxyEvent, sessionId: string): Promise<Response> {
+		let cicSession;
+		try {
+			this.logger.debug("IN processRequest");
+			const bodyParsed = JSON.parse(event.body as string);
+			cicSession = new CicSession(bodyParsed);
+			await this.validationHelper.validateModel(cicSession, this.logger);
+			this.logger.debug("CIC Session is  " + JSON.stringify(cicSession));
+		} catch (error) {
+			return new Response(HttpCodesEnum.BAD_REQUEST, "Missing mandatory fields in the request payload");
+		}
 
-  	const session = await this.cicService.getSessionById(sessionId);
+		const session = await this.cicService.getSessionById(sessionId);
 
-  	if (session != null) {
-  		this.logger.info("found session", JSON.stringify(session));
-  		this.metrics.addMetric("found session", MetricUnits.Count, 1);
-  		this.logger.debug("Session is " + JSON.stringify(session));
-  		await this.cicService.saveCICData(sessionId, cicSession);
-  		const authCode = randomUUID();
-  		await this.cicService.setAuthorizationCode(sessionId, authCode);
-  		const cicResp = new CicResponse({
-  			authorizationCode: authCode,
-  			redirectUri: session?.redirectUri,
-  			state: session?.state,
-  		});
+		if (session != null) {
+			this.logger.info("found session", JSON.stringify(session));
+			this.metrics.addMetric("found session", MetricUnits.Count, 1);
+			this.logger.debug("Session is " + JSON.stringify(session));
+			await this.cicService.saveCICData(sessionId, cicSession);
+			const authCode = randomUUID();
+			await this.cicService.setAuthorizationCode(sessionId, authCode);
+			const cicResp = new CicResponse({
+				authorizationCode: authCode,
+				redirectUri: session?.redirectUri,
+				state: session?.state,
+			});
 
-  		return new Response(StatusCodes.OK, JSON.stringify(cicResp));
-  	} else {
-  		return new Response(StatusCodes.NOT_FOUND, `No session found with the session id: ${sessionId}`);
-  	}
-  }
+			return new Response(HttpCodesEnum.OK, JSON.stringify(cicResp));
+		} else {
+			return new Response(HttpCodesEnum.UNAUTHORIZED, `No session found with the session id: ${sessionId}`);
+		}
+	}
 }
