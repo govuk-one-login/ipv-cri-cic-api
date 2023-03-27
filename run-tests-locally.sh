efae66730bd40e3efd7d939a8e23db6e7feb07f1
AyeshaKhanGDS <110675931+AyeshaKhanGDS@users.noreply.github.com>
2023-03-27 16:37:08 +0100

    F2F-134e: Removing run tests script from deploy folder back to root (#133)
    * Removing run tests script from deploy folder back to root
    
    * Removed reference to working directory as test scripts have been placed in root folder
    
    * Removed workflow-directory from build

---
+++ run-tests-locally.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+if [ $# -ge 1 ] && [ -n "$1" ]
+then
+    echo "Running test container against $1"
+
+    export SAM_STACK=$1 
+    export AWS_REGION="eu-west-2"
+    export TEST_REPORT_DIR="results"
+    export ENVIRONMENT="dev"
+
+    mkdir -p results # The directory on the host where the test results will be outputted.
+
+    aws cloudformation describe-stacks --stack-name "$SAM_STACK" --region "$AWS_REGION" --query 'Stacks[0].Outputs[].{key: OutputKey, value: OutputValue}' --output text > cf-output.txt
+    eval $(awk '{ printf("export CFN_%s=\"%s\"\n", $1, $2) }' cf-output.txt)
+    awk '{ printf("CFN_%s=\"%s\"\n", $1, $2) }' cf-output.txt > docker_vars.env
+    echo TEST_REPORT_DIR="$TEST_REPORT_DIR" >> docker_vars.env
+    echo TEST_REPORT_ABSOLUTE_DIR="$TEST_REPORT_DIR" >> docker_vars.env
+    echo TEST_ENVIRONMENT="$ENVIRONMENT" >> docker_vars.env
+    echo ENVIRONMENT="$ENVIRONMENT" >> docker_vars.env
+    echo SAM_STACK_NAME="$SAM_STACK" >> docker_vars.env
+    
+    docker build -f Dockerfile -t testcontainer .
+    docker run --env-file docker_vars.env -v $(pwd)/results:/results testcontainer
+else    
+    echo "Please ensure you've got a stack name as the first argument after ./run_tests_locally.sh..."
+    echo "E.g. ./run_tests_locally.sh cri-cic-api"
+fi
