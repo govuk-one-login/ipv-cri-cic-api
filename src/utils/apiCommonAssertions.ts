6f577f4e9fb353035fb6b1c1a1f7d75cf5c1afae
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-03-30 16:26:42 +0100

    add missing typedef to call-signatures

--- src/utils/apiCommonAssertions.ts
+++ src/utils/apiCommonAssertions.ts
@@ -1,18 +1,18 @@
-export function assert200OK({ status, statusText }: { status: number; statusText: string }): void {
+export function assert200OK(status: number, statusText: string): void {
 	expect(status).toBe(200);
 	expect(statusText).toBe("OK");
 }
-export function assert201Created({ status, statusText }: { status: number; statusText: string }): void {
+export function assert201Created(status: number, statusText: string): void {
 	expect(status).toBe(201);
 	expect(statusText).toBe("Created");
 }
 
-export function assert400BadRequest({ status, statusText }: { status: number; statusText: string }): void {
+export function assert400BadRequest(status: number, statusText: string): void {
 	expect(status).toBe(400);
 	expect(statusText).toBe("Bad Request");
 }
 
-export function assert401Unauthorized({ status, statusText }: { status: number; statusText: string }): void {
+export function assert401Unauthorized(status: number, statusText: string): void {
 	expect(status).toBe(401);
 	expect(statusText).toBe("Unauthorized");
 }
