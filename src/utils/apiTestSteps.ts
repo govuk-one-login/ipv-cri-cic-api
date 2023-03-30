a0085a9d8a37c9b52eeb73e0319a30f5b3b0d03c
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-03-30 15:24:40 +0100

    get rid of whitespaces

--- src/utils/apiTestSteps.ts
+++ src/utils/apiTestSteps.ts
@@ -1,35 +1,35 @@
 import axios from "axios";
 const API_INSTANCE = axios.create({ baseURL:"https://unb2uhs7rl.execute-api.eu-west-2.amazonaws.com/dev" });
-​
+
 export async function stubStartPost(body:any):Promise<any> {
 	try {
-​
+
 		const path = "https://erveje5km8.execute-api.eu-west-2.amazonaws.com/dev/start";
 		const postRequest = await axios.post(`${path}`, body);
 		return postRequest;
-​
+
 	} catch (error: any) {
 		console.log(`Error response from stub: ${error}`);
 		return error.response;
 	}
 }
-​
+
 export async function sessionPost(clientId?: string, request?: string):Promise<any> {
 	try {
-​
+
 		const path = "/session";
 		const postRequest = await API_INSTANCE.post(`${path}`, { client_id: clientId, request });
 		return postRequest;
-​
+
 	} catch (error: any) {
 		console.log(`Error response from stub: ${error}`);
 		return error.response;
 	}
 }
-​
+
 export async function claimedIdentityPost(given_names?: string, family_names?: string, dob?: string, doc_selected?: string, doe?: string, sessID?: string):Promise<any> {
 	try {
-​
+
 		const path = "/claimedIdentity";
 		const headers = { "x-govuk-signin-session-id": sessID };
 		const postRequest = await API_INSTANCE.post(`${path}`, {
@@ -43,31 +43,31 @@ export async function claimedIdentityPost(given_names?: string, family_names?: s
 			headers,
 		});
 		return postRequest;
-​
+
 	} catch (error: any) {
 		console.log(`Error response from stub: ${error}`);
 		return error.response;
 	}
 }
-​
+
 export async function authorizationGet(sessionId: string):Promise<any> {
 	try {
-​
+		
 		const path = "/authorization";
 		const headers = { "session-id": sessionId };
 		const postRequest = await API_INSTANCE.get(`${path}`, { headers });
 		return postRequest;
-​
+
 	} catch (error: any) {
 		console.log(`Error response from stub: ${error}`);
 		return error.response;
 	}
 }   
-​
+
 export async function tokenPost(authCode?: any, redirectUri?: any ):Promise<any> {
 	try {
 		const path = "/token";
-​
+
 		const data = `code=${authCode}&grant_type=authorization_code&redirect_uri=${redirectUri}`;
 		const postRequest = await API_INSTANCE.post(`${path}`, data, { headers:{ "Content-Type" : "text/plain" } });
         
@@ -78,7 +78,7 @@ export async function tokenPost(authCode?: any, redirectUri?: any ):Promise<any>
 		return error.response;
 	}
 }
-​
+
 export async function userInfoPost(accessToken?: any):Promise<any> {
 	try {
 		const path = "/userInfo";
@@ -92,7 +92,7 @@ export async function userInfoPost(accessToken?: any):Promise<any> {
 		return error.response;
 	}
 }
-​
+
 export async function wellKnownGet():Promise<any> {
 	try {
 		const path = "/.well-known/jwks.json";
