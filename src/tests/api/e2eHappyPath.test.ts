a6c2a3cf86daa335c335ad4b18cf7c741cd01afc
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-04-06 10:41:31 +0100

    CIC BE Test Refactoring

---
+++ src/tests/api/e2eHappyPath.test.ts
@@ -0,0 +1,65 @@
+import { assertStatusCode } from "../../utils/apiHelper";
+import { authorizationGet, sessionPost, stubStartPost, claimedIdentityPost, tokenPost, startStubServiceAndReturnSessionId, wellKnownGet, userInfoPost } from "../../utils/apiTestSteps";
+import * as dataSlim from "../../data/happyPathSlim.json";
+import * as dataBjorn from "../../data/happyPathBjörn.json";
+
+
+describe("E2E Happy Path Tests Slim", () => {
+	let sessionId: any;
+	beforeAll(async () => {
+		const sessionResponse = await startStubServiceAndReturnSessionId();
+		assertStatusCode(200, sessionResponse.status, sessionResponse.statusText);
+		sessionId = sessionResponse.data.session_id;
+	});
+
+	it("E2E Happy Path Journey - User Info Slim", async () => {
+		expect(sessionId).toBeTruthy();
+		// Claimed Identity
+		const calimedIdentityResponse = await claimedIdentityPost(dataSlim.firstName, dataSlim.lastName, dataSlim.dateOfBirth, dataSlim.identityType, dataSlim.dateOfExpiry, sessionId);
+		assertStatusCode(200, calimedIdentityResponse.status, calimedIdentityResponse.statusText);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		assertStatusCode(200, authResponse.status, authResponse.statusText);
+		// Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		assertStatusCode(201, tokenResponse.status, tokenResponse.statusText);
+		// Post User Info
+		const userInfoResponse = await userInfoPost(tokenResponse.data.access_token);
+		assertStatusCode(200, userInfoResponse.status, userInfoResponse.statusText);
+	});
+
+
+	it("E2E Happy Path Journey - Well Known", async () => {
+		// Well Known
+		const wellKnownResponse = await wellKnownGet();
+		assertStatusCode(200, wellKnownResponse.status, wellKnownResponse.statusText);
+	});
+
+
+});
+
+describe("E2E Happy Path Tests Björn", () => {
+	let sessionId: any;
+	beforeAll(async () => {
+		const sessionResponse = await startStubServiceAndReturnSessionId();
+		assertStatusCode(200, sessionResponse.status, sessionResponse.statusText);
+		sessionId = sessionResponse.data.session_id;
+	});
+
+	it("E2E Happy Path Journey - User Info Björn", async () => {
+		expect(sessionId).toBeTruthy();
+		// Claimed Identity
+		const calimedIdentityResponse = await claimedIdentityPost(dataBjorn.firstName, dataBjorn.lastName, dataBjorn.dateOfBirth, dataBjorn.identityType, dataBjorn.dateOfExpiry, sessionId);
+		assertStatusCode(200, calimedIdentityResponse.status, calimedIdentityResponse.statusText);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		assertStatusCode(200, authResponse.status, authResponse.statusText);
+		// Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		assertStatusCode(201, tokenResponse.status, tokenResponse.statusText);
+		// Post User Info
+		const userInfoResponse = await userInfoPost(tokenResponse.data.access_token);
+		assertStatusCode(200, userInfoResponse.status, userInfoResponse.statusText);
+	});
+
+});
--- src/utils/apiTestSteps.ts
+++ src/utils/apiTestSteps.ts
@@ -1,108 +1,83 @@
 import axios from "axios";
-const API_INSTANCE = axios.create({ baseURL:"https://unb2uhs7rl.execute-api.eu-west-2.amazonaws.com/dev" });
+import { constants } from "./ApiConstants";
+import { assertStatusCode, post, get } from "../utils/apiHelper";
+const API_INSTANCE = axios.create({ baseURL:constants.DEV_CRI_CIC_API_URL });
 
-export async function stubStartPost(body:any):Promise<any> {
-	try {
+export async function startStubServiceAndReturnSessionId(): Promise<any> {
+	const stubResponse = await stubStartPost();
+	const postRequest = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
 
-		const path = "https://erveje5km8.execute-api.eu-west-2.amazonaws.com/dev/start";
-		const postRequest = await axios.post(`${path}`, body);
-		return postRequest;
+	return postRequest;
+}
 
-	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
-		return error.response;
-	}
+export async function stubStartPost():Promise<any> {
+	const path = constants.DEV_IPV_STUB_URL;
+	const postRequest = await post(axios, `${path}`, { target:constants.DEV_CRI_CIC_API_URL }, null );
+	assertStatusCode(201, postRequest.status, postRequest.statusText);
+	return postRequest;
 }
 
 export async function sessionPost(clientId?: string, request?: string):Promise<any> {
+	const path = "/session";
 	try {
-
-		const path = "/session";
-		const postRequest = await API_INSTANCE.post(`${path}`, { client_id: clientId, request });
+		const postRequest = await post(API_INSTANCE, path, { client_id: clientId, request }, null);
 		return postRequest;
-
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }
 
-export async function claimedIdentityPost(given_names?: string, family_names?: string, dob?: string, doc_selected?: string, doe?: string, sessID?: string):Promise<any> {
+export async function claimedIdentityPost(given_names?: string, family_names?: string, dob?: string, doc_selected?: string, doe?: string, sessionId?: string):Promise<any> {
+	const path = "/claimedIdentity";
 	try {
-
-		const path = "/claimedIdentity";
-		const headers = { "x-govuk-signin-session-id": sessID };
-		const postRequest = await API_INSTANCE.post(`${path}`, {
-			given_names,
-			family_names,
-			date_of_birth: dob,
-			document_selected: doc_selected,
-			date_of_expiry: doe,
-		},
-		{
-			headers,
-		});
+		const postRequest = await post(API_INSTANCE, "/claimedIdentity", { given_names, family_names, date_of_birth: dob, document_selected: doc_selected, date_of_expiry: doe }, { headers:{ "x-govuk-signin-session-id": sessionId } });
 		return postRequest;
-
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }
 
-export async function authorizationGet(sessionId: string):Promise<any> {
+export async function authorizationGet(sessionId: any):Promise<any> {
+	const path = "/authorization";
 	try {
-		
-		const path = "/authorization";
-		const headers = { "session-id": sessionId };
-		const postRequest = await API_INSTANCE.get(`${path}`, { headers });
-		return postRequest;
-
+		const getRequest = await get(API_INSTANCE, "/authorization", { headers:{ "session-id": sessionId } });
+		return getRequest;
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }   
 
 export async function tokenPost(authCode?: any, redirectUri?: any ):Promise<any> {
+	const path = "/token";
 	try {
-		const path = "/token";
-
-		const data = `code=${authCode}&grant_type=authorization_code&redirect_uri=${redirectUri}`;
-		const postRequest = await API_INSTANCE.post(`${path}`, data, { headers:{ "Content-Type" : "text/plain" } });
-        
+		const postRequest = await post(API_INSTANCE, "/token", `code=${authCode}&grant_type=authorization_code&redirect_uri=${redirectUri}`, { headers:{ "Content-Type" : "text/plain" } });
 		return postRequest;
-        
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }
 
 export async function userInfoPost(accessToken?: any):Promise<any> {
+	const path = "/userInfo";
 	try {
-		const path = "/userInfo";
-        
-		const postRequest = await API_INSTANCE.post(path, null, { headers: { "Authorization": `Bearer ${accessToken}` } });
-        
+		const postRequest = await post(API_INSTANCE, "/userInfo", null, { headers: { "Authorization": `Bearer ${accessToken}` } });
 		return postRequest;
-        
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }
 
 export async function wellKnownGet():Promise<any> {
+	const path = "/.well-known/jwks.json";
 	try {
-		const path = "/.well-known/jwks.json";
-        
-		const postRequest = await API_INSTANCE.get(path);
-        
-		return postRequest;
-        
+		const getRequest = await get(API_INSTANCE, "/.well-known/jwks.json", null);	return getRequest;
 	} catch (error: any) {
-		console.log(`Error response from stub: ${error}`);
+		console.log(`Error response from ${path} endpoint: ${error}`);
 		return error.response;
-	}
+	} 
 }
