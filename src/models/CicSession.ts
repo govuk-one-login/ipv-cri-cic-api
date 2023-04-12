741c1b28d1637f8443b896d1707c331d2012d70c
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-04-12 10:06:19 +0100

    Removed references to 'document selected' and 'document expiry date' from F2F

--- src/models/CicSession.ts
+++ src/models/CicSession.ts
@@ -1,38 +1,28 @@
 import {
-	ArrayNotEmpty,
-	IsArray,
-	IsISO8601,
-	IsNotEmpty,
-	IsString,
+  ArrayNotEmpty,
+  IsArray,
+  IsISO8601,
+  IsNotEmpty,
+  IsString,
 } from "class-validator";
 import { ICicSession } from "./ISessionItem";
 
 export class CicSession implements ICicSession {
-	constructor(data: CicSession) {
-		this.given_names = data.given_names!;
-		this.family_names = data.family_names!;
-		this.date_of_birth = data.date_of_birth!;
-		this.document_selected = data.document_selected!;
-		this.date_of_expiry = data.date_of_expiry!;
-	}
+  constructor(data: CicSession) {
+    this.given_names = data.given_names!;
+    this.family_names = data.family_names!;
+    this.date_of_birth = data.date_of_birth!;
+  }
 
   @IsArray()
   @ArrayNotEmpty()
-  given_names: string [];
+  given_names: string[];
 
   @IsISO8601()
   @IsNotEmpty()
   date_of_birth: string;
 
-  @IsString()
-  @IsNotEmpty()
-  document_selected: string;
-
-  @IsISO8601()
-  @IsNotEmpty()
-  date_of_expiry: string;
-
   @IsArray()
   @ArrayNotEmpty()
-  family_names: string [];
+  family_names: string[];
 }
