553bc86c2ab415b050fd2d7b4b0a5904d00b9f6d
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-04-12 15:22:13 +0100

    Fixed lint issue

--- src/models/CicSession.ts
+++ src/models/CicSession.ts
@@ -1,18 +1,18 @@
 import {
-  ArrayNotEmpty,
-  IsArray,
-  IsISO8601,
-  IsNotEmpty,
-  IsString,
+	ArrayNotEmpty,
+	IsArray,
+	IsISO8601,
+	IsNotEmpty,
+	IsString,
 } from "class-validator";
 import { ICicSession } from "./ISessionItem";
 
 export class CicSession implements ICicSession {
-  constructor(data: CicSession) {
-    this.given_names = data.given_names!;
-    this.family_names = data.family_names!;
-    this.date_of_birth = data.date_of_birth!;
-  }
+	constructor(data: CicSession) {
+		this.given_names = data.given_names!;
+		this.family_names = data.family_names!;
+		this.date_of_birth = data.date_of_birth!;
+	}
 
   @IsArray()
   @ArrayNotEmpty()
