9dcde8c7cae30bdd96583935b3545447323d1464
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-04-12 10:49:21 +0100

    updating ApiConstants class

--- src/tests/utils/apiConstants.ts
+++ src/tests/utils/apiConstants.ts
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 export const constants = {
 	DEV_CRI_CIC_API_URL: process.env.DEV_CRI_CIC_API_URL,
 	DEV_IPV_STUB_URL: process.env.DEV_IPV_STUB_URL,
