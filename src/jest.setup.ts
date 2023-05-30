4433269da4e526b46091ee12cff264e69f084b7e
Viveak Vadivelkarasan <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-05-30 14:19:58 +0100

    [F2F-672] - Add expiry check on getCICSessions (#193)

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -4,5 +4,5 @@ process.env.ISSUER = 'https://XXX-c.env.account.gov.uk'
 process.env.TXMA_QUEUE_URL = "MYQUEUE"
 process.env.CLIENT_CONFIG = '[{"jwksEndpoint":"https://api.identity.account.gov.uk/.well-known/jwks.json","clientId":"ipv-core-stub","redirectUri":"http://localhost:8085/callback"}]'
 process.env.ENCRYPTION_KEY_IDS = 'EncryptionKeyArn'
-process.env.AUTH_SESSION_TTL = '604800'
+process.env.AUTH_SESSION_TTL = '950400'
 process.env.REGION = 'eu-west-2'
