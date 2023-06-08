3a1448169d97c28a869ce278e443492c39b19e47
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2023-06-08 13:25:35 +0100

    Removing body validation for /token

--- src/services/AccessTokenRequestProcessor.ts
+++ src/services/AccessTokenRequestProcessor.ts
@@ -11,6 +11,7 @@ import { AccessTokenRequestValidationHelper } from "../utils/AccessTokenRequestV
 import { ISessionItem } from "../models/ISessionItem";
 import { absoluteTimeNow } from "../utils/DateTimeUtils";
 import { Constants } from "../utils/Constants";
+import {MessageCodes} from "../models/enums/MessageCodes";
 
 const SESSION_TABLE = process.env.SESSION_TABLE;
 const KMS_KEY_ARN = process.env.KMS_KEY_ARN;
@@ -59,6 +60,10 @@ export class AccessTokenRequestProcessor {
     				return new Response(HttpCodesEnum.UNAUTHORIZED, `No session found by authorization code: ${requestPayload.code}`);
     			}
     		} catch (err) {
+				this.logger.error("Error while retrieving the session", {
+					messageCode: MessageCodes.SESSION_NOT_FOUND,
+					error: err
+				});
     			return new Response(HttpCodesEnum.UNAUTHORIZED, "Error while retrieving the session");
     		}
 
