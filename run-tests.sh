f15930f52e53e7bbf969e6329ae38273ab563eb6
mudassar jafer <mjafer@deloitte.co.uk>
2023-06-12 15:50:39 +0100

    Updated run-test.sh to copy tests reports to correct directory

--- run-tests.sh
+++ run-tests.sh
@@ -10,10 +10,12 @@ declare error_code
 # shellcheck disable=SC2154
 #The CFN variables seem to include quotes when used in tests these must be removed before assigning them.
 CFN_CICBackendURL_NoQuotes=$(remove_quotes "$CFN_CICBackendURL")
-export DEV_CRI_CIC_API_URL=`echo ${CFN_CICBackendURL_NoQuotes%/}`
+export DEV_CRI_CIC_API_URL=$(echo ${CFN_CICBackendURL_NoQuotes%/})
 export DEV_IPV_STUB_URL=$(remove_quotes $CFN_CICIPVStubExecuteURL)/start
 
 cd ./src; npm run test:e2e
 error_code=$?
+
+cp -rf results $TEST_REPORT_ABSOLUTE_DIR
 
 exit $error_code
