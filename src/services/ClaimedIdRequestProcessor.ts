976968aea0370d9457df386fedddacf4d7c28a65
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-06-28 13:35:50 +0100

    [F2F-847] Hardens logs and stops logging PII

--- src/services/ClaimedIdRequestProcessor.ts
+++ src/services/ClaimedIdRequestProcessor.ts
@@ -10,6 +10,7 @@ import { HttpCodesEnum } from "../utils/HttpCodesEnum";
 import { absoluteTimeNow } from "../utils/DateTimeUtils";
 import { createDynamoDbClient } from "../utils/DynamoDBFactory";
 import { AuthSessionState } from "../models/enums/AuthSessionState";
+import { MessageCodes } from "../models/enums/MessageCodes";
 
 const SESSION_TABLE = process.env.SESSION_TABLE;
 const PERSON_IDENTITY_TABLE_NAME = process.env.PERSON_IDENTITY_TABLE_NAME;
@@ -27,7 +28,9 @@ export class ClaimedIdRequestProcessor {
 
 	constructor(logger: Logger, metrics: Metrics) {
 		if (!SESSION_TABLE || !PERSON_IDENTITY_TABLE_NAME) {
-			logger.error("Environment variable SESSION_TABLE or PERSON_IDENTITY_TABLE_NAME is not configured");
+			logger.error("Environment variable SESSION_TABLE or PERSON_IDENTITY_TABLE_NAME is not configured", {
+				messageCode: MessageCodes.MISSING_CONFIGURATION,
+			});
 			throw new AppError("Service incorrectly configured", 500);
 		}
 		this.logger = logger;
@@ -46,15 +49,19 @@ export class ClaimedIdRequestProcessor {
 	async processRequest(event: APIGatewayProxyEvent, sessionId: string): Promise<Response> {
 		let cicSession;
 		try {
-			this.logger.debug("IN processRequest");
 			const bodyParsed = JSON.parse(event.body as string);
 			// Convert given_names and family_names string into string[]
 			bodyParsed.given_names = bodyParsed.given_names.split(" ");
 			bodyParsed.family_names = bodyParsed.family_names.split(" ");
+		
 			cicSession = new CicSession(bodyParsed);
 			await this.validationHelper.validateModel(cicSession, this.logger);
 			this.logger.debug({ message: "CIC Session is", cicSession });
 		} catch (error) {
+			this.logger.error("Missing mandatory fields in the request payload", {
+				error,
+				messageCode: MessageCodes.PAYLOAD_VALIDATION_FAILED,
+			});
 			return new Response(HttpCodesEnum.BAD_REQUEST, "Missing mandatory fields in the request payload");
 		}
 
@@ -62,19 +69,25 @@ export class ClaimedIdRequestProcessor {
 
 		if (session != null) {
 			if (session.expiryDate < absoluteTimeNow()) {
+				this.logger.error("Session has expired", { messageCode: MessageCodes.EXPIRED_SESSION });
 				return new Response(HttpCodesEnum.UNAUTHORIZED, `Session with session id: ${sessionId} has expired`);
 			}
 
-			this.logger.info({ message: "found session", session });
 			this.metrics.addMetric("Found session", MetricUnits.Count, 1);
 
 			if (session.authSessionState !== AuthSessionState.CIC_SESSION_CREATED) {
-				this.logger.warn(`Session is in the wrong state: ${session.authSessionState}, expected state should be ${AuthSessionState.CIC_SESSION_CREATED}`);
+				this.logger.error(`Session is in the wrong state: ${session.authSessionState}, expected state should be ${AuthSessionState.CIC_SESSION_CREATED}`, { 
+					messageCode: MessageCodes.INCORRECT_SESSION_STATE,
+				});
 				return new Response(HttpCodesEnum.UNAUTHORIZED, `Session is in the wrong state: ${session.authSessionState}`);
 			}
+
 			await this.cicService.saveCICData(sessionId, cicSession, session.expiryDate);
 			return new Response(HttpCodesEnum.OK, "");
 		} else {
+			this.logger.error("No session found for session id", {
+				messageCode: MessageCodes.SESSION_NOT_FOUND,
+			});
 			return new Response(HttpCodesEnum.UNAUTHORIZED, `No session found with the session id: ${sessionId}`);
 		}
 	}
