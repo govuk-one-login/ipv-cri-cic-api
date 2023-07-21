10bb36d94b8c462cf9c0486e7c51a47874185c7d
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2023-07-21 18:56:54 +0100

    F2F-926 Updates (#261)
    * F2F-926 Updates

--- src/tests/api/e2eHappyPath.test.ts
+++ src/tests/api/e2eHappyPath.test.ts
@@ -1,65 +1,45 @@
-import * as dataSlim from "../data/happyPathSlim.json";
-import * as dataBjorn from "../data/happyPathBjörn.json";
-import { authorizationGet, claimedIdentityPost, tokenPost, startStubServiceAndReturnSessionId, wellKnownGet, userInfoPost, validateJwtToken, validateWellKnownReponse } from "../utils/ApiTestSteps";
+import dataSlim from "../data/happyPathSlim.json";
+import dataBjorn from "../data/happyPathBjörn.json";
+import dataManuel from "../data/happyPathManuel.json";
+import dataBillyJoe from "../data/happyPathBillyJoe.json";
+
+import { authorizationGet, claimedIdentityPost, tokenPost, startStubServiceAndReturnSessionId, userInfoPost, validateJwtToken, wellKnownGet, validateWellKnownReponse } from "../utils/ApiTestSteps";
 
 
-describe("E2E Happy Path Tests Slim", () => {
-	let sessionId: any;
-	beforeAll(async () => {
+describe("E2E Happy Path Tests", () => {
+
+	it.each([
+		[dataSlim],
+		[dataBjorn],
+		[dataManuel],
+		[dataBillyJoe],
+	])("E2E Happy Path Journey - User Info", async (userData: any) => {
 		const sessionResponse = await startStubServiceAndReturnSessionId();
-		sessionId = sessionResponse.data.session_id;
-	});
-
-	it("E2E Happy Path Journey - User Info Slim", async () => {
+		console.log(sessionResponse.data);
+		const sessionId = sessionResponse.data.session_id;
+		console.log(sessionId);
 		expect(sessionId).toBeTruthy();
 		// Claimed Identity
-		const calimedIdentityResponse = await claimedIdentityPost(dataSlim.firstName, dataSlim.lastName, dataSlim.dateOfBirth, sessionId);
+		const calimedIdentityResponse = await claimedIdentityPost(userData.firstName, userData.lastName, userData.dateOfBirth, sessionId);
 		expect(calimedIdentityResponse.status).toBe(200);
 		// Authorization
 		const authResponse = await authorizationGet(sessionId);
+		console.log(authResponse.data);
 		expect(authResponse.status).toBe(200);
 		// Post Token
 		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
 		expect(tokenResponse.status).toBe(200);
 		// Post User Info
 		const userInfoResponse = await userInfoPost(tokenResponse.data.access_token);
-		validateJwtToken(JSON.stringify(userInfoResponse.data), dataSlim);
+		validateJwtToken(JSON.stringify(userInfoResponse.data), userData);
 		expect(userInfoResponse.status).toBe(200);
 	});
-
-});
-
-describe("E2E Happy Path Tests Björn", () => {
-	let sessionId: any;
-	beforeAll(async () => {
-		const sessionResponse = await startStubServiceAndReturnSessionId();
-		sessionId = sessionResponse.data.session_id;
-	});
-
-	it("E2E Happy Path Journey - User Info Björn", async () => {
-		expect(sessionId).toBeTruthy();
-		// Claimed Identity
-		const calimedIdentityResponse = await claimedIdentityPost(dataBjorn.firstName, dataBjorn.lastName, dataBjorn.dateOfBirth, sessionId);
-		expect(calimedIdentityResponse.status).toBe(200);
-		// Authorization
-		const authResponse = await authorizationGet(sessionId);
-		expect(authResponse.status).toBe(200);
-		// Post Token
-		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
-		expect(tokenResponse.status).toBe(200);
-		// Post User Info
-		const userInfoResponse = await userInfoPost(tokenResponse.data.access_token);
-		validateJwtToken(JSON.stringify(userInfoResponse.data), dataBjorn);
-		expect(userInfoResponse.status).toBe(200);
-	});
-
 });
 
 describe("E2E Happy Path Well Known Endpoint", () => {
 	it("E2E Happy Path Journey - Well Known", async () => {
 		// Well Known
 		const wellKnownResponse = await wellKnownGet();
-		console.log(wellKnownResponse.data);
 		validateWellKnownReponse(wellKnownResponse.data);
 		expect(wellKnownResponse.status).toBe(200);
 	});
