3c11082abb7380e3d50fd96b242892554180b7ed
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2024-02-29 15:12:54 +0000

    Updating user data to match pactfile responses.

--- src/tests/contract/routes/UserInfoRoute.ts
+++ src/tests/contract/routes/UserInfoRoute.ts
@@ -10,6 +10,7 @@ process.env.PERSON_IDENTITY_TABLE_NAME = "person-identity-table";
 process.env.TXMA_QUEUE_URL = "txma-queue";
 process.env.USE_MOCKED = "true";
 process.env.KMS_KEY_ARN = "kid";
+process.env.ISSUER = "dummyClaimedIdentityComponentId";
 
 export const userInfoRouter = asyncify(express.Router());
 
