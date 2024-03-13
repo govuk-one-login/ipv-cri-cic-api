97bebebfc3274ce49ab078384021e036fd20b642
MohamedHassan101 <mohamed.hassan@digital.cabinet-office.gov.uk>
2024-03-13 09:30:00 +0000

    updated the Abort After Claimed Identity Request test

--- src/tests/api/HappyPath.test.ts
+++ src/tests/api/HappyPath.test.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable max-len */
+
 /* eslint-disable max-lines-per-function */
 import userData from "../data/happyPathSlim.json";
 import { constants } from "./ApiConstants";
@@ -73,9 +74,14 @@ describe("Happy path tests", () => {
 			expect(response.status).toBe(200);
 			expect(response.data).toBe("Session has been aborted");
 
+			const url = new URL(decodeURIComponent(response.headers.location));
+			expect(url.searchParams.has("error")).toBe(true);
+			expect(url.searchParams.has("state")).toBe(true);
+			expect(url.searchParams.get("error")).toBe("access_denied");
+
 			await getSessionAndVerifyKey(sessionId, constants.DEV_CIC_SESSION_TABLE_NAME, "authSessionState", "CIC_CRI_SESSION_ABORTED");
 
-			const allTxmaEventBodies = await getTxmaEventsFromTestHarness(sessionId, 4);
+			const allTxmaEventBodies = await getTxmaEventsFromTestHarness(sessionId, 2);
 
 			validateTxMAEventData({ eventName: "CIC_CRI_START", schemaName: "CIC_CRI_START_SCHEMA" }, allTxmaEventBodies);
 
@@ -84,13 +90,7 @@ describe("Happy path tests", () => {
 			expect(response.headers).toBeTruthy();
 			expect(response.headers.location).toBeTruthy();
 
-			const responseURI = decodeURIComponent(response.headers.location);
-			const responseURIParameters = new URLSearchParams(responseURI);
-			expect(responseURIParameters.has("error")).toBe(true);
-			expect(responseURIParameters.has("state")).toBe(true);
-			expect(responseURIParameters.get("error")).toBe("access_denied");
-
-			await getSessionAndVerifyKey(sessionId, constants.DEV_CIC_SESSION_TABLE_NAME, "state", "" + responseURIParameters.get("state"));
+			await getSessionAndVerifyKey(sessionId, constants.DEV_CIC_SESSION_TABLE_NAME, "state", "" + url.searchParams.get("state"));
 		});
 
 	});
