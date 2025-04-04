64fdff056743550c6e63d9724ceece2e8bca59f9
GDainton <117987734+GDainton@users.noreply.github.com>
2025-04-04 14:45:28 +0100

    [KIWI-1631] - CIC | Platform & BE | New errorCode when well-known JWT Fails Verification (#655)
    * CIC_FAILED_VERIFYING_JWT alarms
    
    * Alarms set up
    
    * Evaluation period edited
    
    * Development log group removed and metric dependency switched to actual session log group
    
    * pagerduty added for critical JWT alarm
    
    * UNEXPECTED_ERROR_VERIFYING_JWT replaced with CIC_FAILED_VERIFYING_JWT
    
    * Unit test fix

--- src/tests/unit/services/SessionRequestProcessor.test.ts
+++ src/tests/unit/services/SessionRequestProcessor.test.ts
@@ -135,7 +135,7 @@ describe("SessionRequestProcessor", () => {
 		expect(logger.error).toHaveBeenCalledWith(
 			expect.anything(),
 			expect.objectContaining({
-				messageCode: "FAILED_VERIFYING_JWT",
+				messageCode: "CIC_FAILED_VERIFYING_JWT",
 			}),
 		);
 	});
@@ -152,7 +152,7 @@ describe("SessionRequestProcessor", () => {
 		expect(logger.error).toHaveBeenCalledWith(
 			expect.anything(),
 			expect.objectContaining({
-				messageCode: "UNEXPECTED_ERROR_VERIFYING_JWT",
+				messageCode: "CIC_FAILED_VERIFYING_JWT",
 			}),
 		);
 	});
