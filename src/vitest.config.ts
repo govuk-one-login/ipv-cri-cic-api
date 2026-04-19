0872339afe8efcaa858d67528dfe8b8721312dbc
Sandy Zhang <sandy.zhang@digital.cabinet-office.gov.uk>
2026-04-19 15:00:01 +0100

    Updated vitest config, added dotenv config and coverage. Clean up package json

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import { defineConfig } from "vitest/config";
 
+dotenv.config();
 export default defineConfig({
   test: {
     globals: true,
@@ -8,6 +10,7 @@ export default defineConfig({
     include: ["**/tests/**/*.test.ts"],
     clearMocks: true,
     coverage: {
+      enabled: true,
       provider: "v8",
       include: ["**/*.ts"],
       exclude: [
