1c4473af79a914a16ffd90c39f259b54c50f7f40
Sophie Kane <sophie.kane@digital.cabinet-office.gov.uk>
2026-04-29 14:51:28 +0100

    KIWI-2641-e Migrate from CJS to ESM update base64 import format

--- src/utils/JwtUtils.ts
+++ src/utils/JwtUtils.ts
@@ -1,15 +1,16 @@
+import * as jose from "node-jose";
 import crypto from "crypto";
 
 export const jwtUtils = {
 
 	// convert non-base64 string or uint8array into base64 encoded string
 	base64Encode(value: string | Uint8Array): string {
-		return Buffer.from(value).toString("base64url");
+		return jose.util.base64url.encode(Buffer.from(value), "utf8");
 	},
 
 	// convert base64 into uint8array
 	base64DecodeToUint8Array(value: string): Uint8Array {
-		return new Uint8Array(Buffer.from(value, "base64url"));
+		return new Uint8Array(jose.util.base64url.decode(value));
 	},
 
 	// convert base64 encoded string into non-base64 string
