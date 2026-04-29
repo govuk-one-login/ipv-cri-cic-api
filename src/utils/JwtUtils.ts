120346d83dbeef492089a75f2953b237cbd1b513
Sophie Kane <sophie.kane@digital.cabinet-office.gov.uk>
2026-04-29 15:12:41 +0100

    KIWI-2641-e Migrate from CJS to ESM chaning from node-jse to jose

--- src/utils/JwtUtils.ts
+++ src/utils/JwtUtils.ts
@@ -1,22 +1,22 @@
-import * as jose from "node-jose";
+import * as jose from "jose";
 import crypto from "crypto";
 
 export const jwtUtils = {
 
 	// convert non-base64 string or uint8array into base64 encoded string
 	base64Encode(value: string | Uint8Array): string {
-		return jose.util.base64url.encode(Buffer.from(value), "utf8");
+    	return jose.base64url.encode(value);
 	},
 
 	// convert base64 into uint8array
 	base64DecodeToUint8Array(value: string): Uint8Array {
-		return new Uint8Array(jose.util.base64url.decode(value));
+    	return jose.base64url.decode(value);
 	},
 
 	// convert base64 encoded string into non-base64 string
 	base64DecodeToString(value: string): string {
-		return Buffer.from(value, "base64url").toString();
-	},
+    	return new TextDecoder().decode(jose.base64url.decode(value));
+  	},
 
 	// convert uint8array into string
 	decode(value: Uint8Array): string {
@@ -32,8 +32,6 @@ export const jwtUtils = {
 
 	// hash string then present output as UTF-8 encoded hexadecimal string
 	getHashedKid(keyId: string): string {
-		const kidBytes = Buffer.from(keyId, "utf8");
-		const hash = crypto.createHash("sha256").update(kidBytes).digest();
-		return Buffer.from(hash).toString("hex");
+		return crypto.createHash("sha256").update(keyId, "utf8").digest("hex");
 	},
 };
